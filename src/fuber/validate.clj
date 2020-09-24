718541f7c9732def5685901efcbdc3409295d9bc
Aman Shah (Software Developer) <aman.shah@enableit.dk>
2020-09-24 20:28:34 +0530

    Added few more validations and updated some

--- src/fuber/validate.clj
+++ src/fuber/validate.clj
@@ -1,16 +1,22 @@
 (ns fuber.validate
-  (:require [clojure.spec.alpha :as s]
-            [fuber.book :refer [cabs-info]]))
-
-(defn license-exists?
-  "Retuns true if license exists else false"
-  [license]
-  (not (empty? (filter #(= license (% :license)) @cabs-info))))
+  (:require [clojure.spec.alpha :as s]))
 
 (s/def ::location
-  (s/and vector? #(= (count %) 2) #(every? int? %)))
+  (s/coll-of number? :kind vector? :count 2))
 
-(s/def ::license
-  (s/and string? #(license-exists? %)))
+(s/def ::license string?)
+  ;(s/and string? #(license-exists? %)))
 
 (s/def ::pink boolean?)
+
+(defn valid-end-trip-params?
+  "Validates if the params provided to end-trip function is valid or not"
+  [lic-num dest]
+  (and (s/valid? ::license lic-num)
+       (s/valid? ::location dest)))
+
+(defn valid-book-trip-params?
+  "Validates if the params provided to book-cab function is valid or not"
+  [source pink]
+  (and (s/valid? ::location source)
+       (s/valid? ::pink pink)))
