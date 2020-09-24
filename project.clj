14dfe03b384de395588c70b6442dd6d4dfe4147e
Aman Shah (Software Developer) <aman.shah@enableit.dk>
2020-09-24 20:27:53 +0530

    Added ring and compojure dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins       [[lein-ring "0.12.5"]]
   :main ^:skip-aot fuber.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
