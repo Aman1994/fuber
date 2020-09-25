d44d2fbfd0a7e90599538fc9f3a620016abda07b
Aman Shah (Software Developer) <aman.shah@enableit.dk>
2020-09-25 19:24:00 +0530

    Updated description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject fuber "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject fuber "0.1.0"
+  :description "Backend APIs for cab booking service"
+  :url "https://github.com/Aman1994/fuber"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
