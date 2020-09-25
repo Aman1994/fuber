3026d5889f77b47d5c63122750481ba66b9b370a
Aman Shah (Software Developer) <aman.shah@enableit.dk>
2020-09-25 09:17:20 +0530

    Used redefs, since can't test with random latitude and longitude

--- test/fuber/core_test.clj
+++ test/fuber/core_test.clj
@@ -1,19 +1,36 @@
 (ns fuber.core-test
   (:require [clojure.test :refer :all]
             [fuber.core :refer :all]
-            [clj-http.client :refer [get]]))
+            [clj-http.client :as http]
+            [fuber.book :refer :all]))
 
 (deftest handlers
   (testing "testing endpoints response"
-    (-main)
-    (is (= (select-keys (get "http://localhost:3000/total-amount") [:status :body])
-           {:status 200
-            :body "0"}))
-    (is (= (select-keys (get "http://127.0.0.1:3000/book-trip?source=[1,2]&pink=false")
-                        [:status :body])
-           {:status 200
-            :body "Cab with license number 456abc booked"}))
-    (is (= (select-keys (get "http://127.0.0.1:3000/end-trip?license=456abc&destination=[3 4]")
-                        [:status :body])
-           {:status 200
-            :body "Trip Ended"}))))
+    (with-redefs [cabs-info
+                  (atom [{:license "xyz123" :booked? false :pink? false :location [1 5]}
+                         {:license "pqr123" :booked? false :pink? true :location [2 9]}
+                         {:license "lmn123" :booked? false :pink? false :location [3 21]}])
+                  total-amount (atom 0)]
+     (-main)
+     (is (= (select-keys (http/get "http://localhost:3000/total-amount") [:status :body])
+            {:status 200
+             :body "0"}))
+     (is (= (select-keys (http/get "http://127.0.0.1:3000/book-trip?source=[10,2]&pink=false")
+                         [:status :body])
+            {:status 200
+             :body "Cab with license number xyz123 booked"}))
+     (is (= (select-keys (http/get "http://127.0.0.1:3000/book-trip?source=[10,2]&pink=true")
+                         [:status :body])
+            {:status 200
+             :body "Cab with license number pqr123 booked"}))
+     (is (= (select-keys (http/get "http://127.0.0.1:3000/end-trip?license=xyz123&destination=[3 4]")
+                         [:status :body])
+            {:status 200
+             :body "Trip Ended. Total amount owed = 123.7618681177688"}))
+     (is (= (select-keys (http/get "http://127.0.0.1:3000/end-trip?license=pqr123&destination=[3 4]")
+                         [:status :body])
+            {:status 200
+             :body "Trip Ended. Total amount owed = 128.7618681177688"}))
+     (is (= (select-keys (http/get "http://localhost:3000/total-amount") [:status :body])
+            {:status 200
+             :body "252.52373623553763"})))))
